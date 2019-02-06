2370d97f85d6cc8485145741629934980b6a0d1f
Alessio Occhipinti <alessio.occhipinti@ovalmoney.com>
2019-02-06 11:40:35 +0100

    feat: added index.d.ts for typescript

---
+++ types/index.d.ts
@@ -0,0 +1,39 @@
+type Action = (state: any, ...payload: Array<any>) => any
+type ActionMap = {
+  [actionName: string]: Action,
+}
+type ResolvedAction = (...payload: Array<any>) => void
+type ResolvedActionMap = {
+  [actionName: string]: ResolvedAction,
+}
+
+type Effect = (
+  actions: ResolvedActionMap,
+  effects: ResolvedEffectMap,
+  ...payload: Array<any>
+) => void
+type EffectMap = {
+  [effectName: string]: Effect,
+}
+type ResolvedEffect = (...payload: Array<any>) => void
+type ResolvedEffectMap = {
+  [effectName: string]: ResolvedEffect,
+}
+
+export interface OptionsShape {
+  model?: any,
+  actions?: ActionMap,
+  effects?: EffectMap,
+}
+
+export interface InterfaceShape {
+  state: any,
+  actions: ResolvedActionMap,
+  effects: ResolvedEffectMap,
+}
+
+export interface RenderPropsShape {
+  Consumer: any,
+  Provider: any,
+  Wrapper: any,
+}
