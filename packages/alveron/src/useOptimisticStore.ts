c01118f6efa3d2ee2f97b99d4acc114d16427a91
Robin Weser <contact@weser.io>
2024-07-03 11:08:59 +0200

    fix optimistic stor

--- packages/alveron/src/useOptimisticStore.ts
+++ packages/alveron/src/useOptimisticStore.ts
@@ -1,3 +1,23 @@
-import useOptimisticStoreWithMiddleware from './useOptimisticStoreWithMiddleware'
+import { useOptimistic } from 'react'
 
-export default useOptimisticStoreWithMiddleware()
+import useStoreFactory, { Middleware } from './useStoreFactory'
+
+export default function useOptimisticStoreWithMiddleware<Model>(
+  middleware: Array<Middleware<Model>> = []
+) {
+  function useState(initialState: Model): [Model, (newState: Model) => void] {
+    const [state, setState] = useOptimistic(initialState, (_: Model, update: Model) => update)
+
+    function setFunctionalState(newState: Model) {
+      if (typeof newState === 'function') {
+        return setState(newState(state))
+      }
+
+      return setState(newState)
+    }
+
+    return [state, setFunctionalState]
+  }
+
+  return useStoreFactory<Model>(useState)(middleware)
+}
